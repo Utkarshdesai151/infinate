29307168de683a06e4e9e6da48f31c97b5417106
Utkarshdesai151 <desaiutkarsh151@gmail.com>
2025-01-29 17:42:12 +0530

    home2

--- js/script.js
+++ js/script.js
@@ -29,3 +29,59 @@ btn2.addEventListener("mousemove", (event) => followCursor(event, btn2));
 btn2.addEventListener("mouseleave", () => {
   btn2.style.transform = "translate(0, 0)"; 
 });
+
+//accordion
+document.addEventListener("DOMContentLoaded", function () {
+  const accordionButtons = document.querySelectorAll(".accordion-button");
+
+  accordionButtons.forEach(button => {
+      button.addEventListener("click", function () {
+          const target = document.querySelector(this.getAttribute("data-target"));
+
+          const allCollapses = document.querySelectorAll(".accordion-collapse");
+          allCollapses.forEach(collapse => {
+              if (collapse !== target) {
+                  collapse.classList.remove("show");
+              }
+          });
+
+         
+          target.classList.toggle("show");
+
+         
+          this.classList.toggle("collapsed");
+      });
+  });
+});
+
+corsole
+
+const list = document.querySelector('.list');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+list.addEventListener('mousedown', (e) => {
+  isDown = true;
+  list.classList.add('active');
+  startX = e.pageX - list.offsetLeft;
+  scrollLeft = list.scrollLeft;
+});
+
+list.addEventListener('mouseleave', () => {
+  isDown = false;
+  list.classList.remove('active');
+});
+
+list.addEventListener('mouseup', () => {
+  isDown = false;
+  list.classList.remove('active');
+});
+
+list.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - list.offsetLeft;
+  const walk = (x - startX) * 2; // Adjust scroll speed
+  list.scrollLeft = scrollLeft - walk;
+});
